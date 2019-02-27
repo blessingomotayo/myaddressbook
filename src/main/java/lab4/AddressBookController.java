118cb7b778c84d9b84f56b2bb07e27e6a756419a
blessingomotayo <blessing.omotayo@carleton.ca>
2019-02-27 00:13:32 -0500

    Fixed test error

--- src/main/java/lab4/AddressBookController.java
+++ src/main/java/lab4/AddressBookController.java
@@ -15,41 +15,65 @@ public class AddressBookController {
     @Autowired
     AddressBookRepository addressBookRepository;
 
+
     @ResponseBody
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String hello() {
         return "Hello from the other side!";
     }
 
-    @ResponseBody
-    @RequestMapping(value = "/addressbook", method = RequestMethod.GET)
-    public AddressBook addressBook() {
-        AddressBook addressBook = new AddressBook();
+    @GetMapping(value = "/addressbook")
+    public String addressBook(Model model) {
+        AddressBook addressBook = new AddressBook(new Long(1));
         addressBookRepository.save(addressBook);
 
-        return addressBook;
+        model.addAttribute("addressbook", addressBook);
+        model.addAttribute("newBuddy", new BuddyInfo());
+
+        return "addressbook";
     }
 
-    @RequestMapping(value = "/addBuddy", method = RequestMethod.GET)
-    public String addBuddy(@RequestParam(value = "id", required = true, defaultValue = "1") long id,
-            @RequestParam(value = "name", required = true, defaultValue = "Blessing") String name,
-                           @RequestParam(value = "phoneNumber", required = true, defaultValue = "123") int phoneNumber,
-                              Model model) {
+    @PostMapping(value = "/addressbook")
+    public String addBuddy(@ModelAttribute BuddyInfo bud, Model model) {
 
         AddressBook addressBook;
 
-        BuddyInfo bud = new BuddyInfo(name, phoneNumber);
         buddyInfoRepository.save(bud);
+        System.out.println(buddyInfoRepository.findByName(bud.getName()));
 
-        addressBook = addressBookRepository.findById(id);
+        addressBook = addressBookRepository.findById(new Long(1)).orElse(new AddressBook(new Long(1)));
         addressBook.addBuddys(bud);
+        System.out.println(addressBookRepository.findById(addressBook.getId()));
+
 
         addressBookRepository.save(addressBook);
 
-        model.addAttribute("name", name);
-        model.addAttribute("phoneNumber", phoneNumber);
-        return "buddyinfo";
+        model.addAttribute("addressbook", addressBook);
+        model.addAttribute("newBuddy", new BuddyInfo());
+        return "addressbook";
     }
+
+//    @ResponseBody
+//    @PostMapping(value = "/addBuddy")
+//    public String addBuddy(@RequestParam(value = "id", required = true, defaultValue = "1") long id,
+//            @RequestParam(value = "name", required = true, defaultValue = "Blessing") String name,
+//                           @RequestParam(value = "phoneNumber", required = true, defaultValue = "123") int phoneNumber,
+//                              Model model) {
+//
+//        AddressBook addressBook;
+//
+//        BuddyInfo bud = new BuddyInfo(name, phoneNumber);
+//        buddyInfoRepository.save(bud);
+//
+//        addressBook = addressBookRepository.findById(id);
+//        addressBook.addBuddys(bud);
+//
+//        addressBookRepository.save(addressBook);
+//
+//        model.addAttribute("name", name);
+//        model.addAttribute("phoneNumber", phoneNumber);
+//        return "buddyinfo";
+//    }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public String addBuddy(@PathVariable("id")long id, Model model) {
